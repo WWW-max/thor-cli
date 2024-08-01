255745c88f30ad4253f5772b46d7026d09e9a681
WWW-max <464711201@qq.com>
2024-08-01 17:40:00 +0800

    feat: 运行thor c 'url' 克隆项目到～/project文件夹中

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,30 @@
 #!/usr/bin/env node
 const { program } = require('commander');
+const gitclone = require('gitclone');
+const childProcess = require('child_process');
 
-program.name('thor-cli').usage('<command> [option]');
+const localPath = '~/project';
 
+/** 描述信息 */
+program.name('thor-cli')
+       .description('A CLI that helps you clone git projects into ~/project wherever you are.')
+       .usage('<command> [option]')
+       .version('1.0.0');
+
+/** 定义命令 */
+program.command('c <url>')
+.description('克隆项目')
+.action((repoURL) => {
+console.log('[ 工程名称 ] >', repoURL);
+gitclone(repoURL, localPath, (error, gitFolder) => {
+    if (error) {
+      console.error('Error cloning repository:', error);
+    } else {
+      console.log('Repository cloned successfully:', gitFolder);
+      childProcess.exec(`code ${gitFolder} `);
+    }
+  });
+});
+
+/** 解析命令 */
 program.parse(process.argv);
